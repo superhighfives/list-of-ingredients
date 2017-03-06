5d8ae3b86956575b8019e39c7212f5b78e7a0234
Charlie Gleason <hi@superhighfives.com>
2017-03-06 17:04:03 +0000

    Updated ingredient to save drink id

--- app/admin/ingredient.rb
+++ app/admin/ingredient.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Ingredient do
-  permit_params :description
+  permit_params :description, :drink_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
